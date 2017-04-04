32b14c851642c312c6d5aee778d3d96f45b0c8cf
HuyHunter <hoangquochuyhqh305@gmail.com>
2017-04-04 21:23:11 +0700

    ok

--- ThuctapNhom/ThuctapNhom/ThuctapNhom/Program.cs
+++ ThuctapNhom/ThuctapNhom/ThuctapNhom/Program.cs
@@ -16,7 +16,7 @@ namespace ThuctapNhom
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormStart());
+            Application.Run(new FormLogin());
         }
     }
 }
