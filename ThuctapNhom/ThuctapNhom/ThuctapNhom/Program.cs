6c4c415d387bd3cae3a123271a4f9fe589ce58a6
HuyHunter <hoangquochuyhqh305@gmail.com>
2017-04-07 19:22:18 +0700

    Revert "ok"
    This reverts commit 719c333facad1a820fd88bfc9341d8acf841714f.

--- ThuctapNhom/ThuctapNhom/ThuctapNhom/Program.cs
+++ ThuctapNhom/ThuctapNhom/ThuctapNhom/Program.cs
@@ -16,7 +16,7 @@ namespace ThuctapNhom
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new FormStart());
         }
     }
 }
