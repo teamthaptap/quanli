cffec544c101f4585f93bec22696ccc44c28e5c2
Tùng Chòe <jeniferchoe2712@gmail.com>
2017-04-15 14:40:16 +0700

    Bạn Tùng lại cập nhật Thư viện và beautifull hơn hết
    Bạn Tùng cx đã làm sẵn nút cập nhật cho các bạn r nhóe cả chức năng đăng
    nhập nữa ahihi . Say oh yeah !!!!!!!!

---
+++ quan li thư viện/QLTV/QLTV/ControlDausach.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationBlocks.Data;
+
+namespace QLTV
+{
+    public partial class ControlDausach : UserControl
+    {
+        public ControlDausach()
+        {
+            InitializeComponent();
+            this.Load += loaddausach;
+        }
+
+        private void loaddausach(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+
+        private void loaddulieu()
+        {
+            dgvHienthi.DataSource = SqlHelper.ExecuteDataset(QLTV.Class.StrConnect.strConnect(), "loaddausach").Tables[0];
+        }
+
+        private void btCapnhat_Click(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+    }
+}
