c87b5e04367410bdb2176756921848e481619dbe
Tùng Chòe <jeniferchoe2712@gmail.com>
2017-04-21 21:07:42 +0700

    Này bạn Huy occho

--- Quản lý thư viện/QLTV/QLTV/Program.cs
+++ Quản lý thư viện/QLTV/QLTV/Program.cs
@@ -16,7 +16,7 @@ namespace QLTV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormLogin());
         }
     }
 }
