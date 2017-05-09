2ce96a1dc05772ad3a75df103686f45d0eff6c5c
vitcon96 <hoangthithuyvan305@gmail.com>
2017-05-09 07:58:52 +0700

    new
    làm 1 tí :v:

--- Quản lý thư viện/QLTV/QLTV/ControlDausach.cs
+++ Quản lý thư viện/QLTV/QLTV/ControlDausach.cs
@@ -13,6 +13,7 @@ namespace QLTV
 {
     public partial class ControlDausach : UserControl
     {
+        string strConnect = @"Data Source =LATITUDE-PC;Initial Catalog = QL_KhachSan; Integrated Security = True";
         public ControlDausach()
         {
             InitializeComponent();
@@ -32,6 +33,47 @@ namespace QLTV
         private void btCapnhat_Click(object sender, EventArgs e)
         {
             loaddulieu();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        public void loadCBBTheLoai()
+        {
+            DataTable dtdd;
+            dtdd = SqlHelper.ExecuteDataset(strConnect, "loadtheloai").Tables[0];
+            cbbTheloai.DataSource = dtdd;
+            cbbTheloai.DisplayMember = "TenTheLoai";
+            cbbTheloai.ValueMember = "MaTheLoai";
+        }
+        public void loadCBBNXB()
+        {
+            DataTable dtdd;
+            dtdd = SqlHelper.ExecuteDataset(strConnect, "loadNXB").Tables[0];
+            cbbNXB.DataSource = dtdd;
+            cbbNXB.DisplayMember = "TenNXB";
+            cbbNXB.ValueMember = "MaNXB";
+        }
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ma = txtMa.Text.Trim();
+                string tacgia = txtTacgia.Text.Trim();
+                string soluong = txtSoluong.Text.Trim();
+                string nhande = txtNhande.Text.Trim();
+                string domat = txtDomat.Text.Trim();
+                string ngonngu = txtNgonngu.Text.Trim();
+                string theloai =cbbTheloai.SelectedValue.ToString();
+                string maNXB = cbbNXB.SelectedValue.ToString();
+                SqlHelper.ExecuteNonQuery(strConnect, "ADDTaiLieu", ma, tacgia, soluong,nhande,domat,ngonngu,theloai,maNXB);
+                MessageBox.Show("thành công");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("lỗi");
+            }
         }
     }
 }
