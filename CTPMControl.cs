399b80ee5621d9a8de47828a6117f581268e5478
HuyHunter <hoangquochuyhqh305@gmail.com>
2017-06-05 02:02:13 +0700

    Của ng ae Tùng Chòe

---
+++ CTPMControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationBlocks.Data;
+using QLTV.Class;
+
+namespace QLTV
+{
+    public partial class CTPMControl : UserControl
+    {
+        public CTPMControl()
+        {
+            InitializeComponent();
+            Load += loadCTPM;
+        }
+        private void loadCTPM(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+
+        private void loaddulieu()
+        {
+            dgvHienthi.DataSource = SqlHelper.ExecuteDataset(QLTV.Class.StrConnect.strConnect(), "loadchitiephieumuon").Tables[0];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                try
+                {
+                    string mapm = cbbPhieu.Text.Trim();
+                    string mas = cbbSach.Text.Trim();
+                    DateTime ngaymuon = DateTime.Parse(dtpNgaymuon.Value.ToString());
+                    DateTime ngaytra = DateTime.Parse(dtpNgaytra.Value.ToString());
+                    DateTime hantra = DateTime.Parse(dtpHantra.Value.ToString());
+                    string ghichu = txtGhiChu.Text.Trim();
+                    SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "addCTPM", mapm, mas, ngaymuon, ngaytra, hantra, ghichu);
+                    MessageBox.Show("Thêm thành công!");
+                    //loaddgv();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Xảy ra lỗi!!!");
+                }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mapm = cbbPhieu.Text.Trim();
+                string mas = cbbSach.Text.Trim();
+                DateTime ngaymuon = DateTime.Parse(dtpNgaymuon.Value.ToString());
+                DateTime ngaytra = DateTime.Parse(dtpNgaytra.Value.ToString());
+                DateTime hantra = DateTime.Parse(dtpHantra.Value.ToString());
+                string ghichu = txtGhiChu.Text.Trim();
+                SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "updateCTPM", mapm, mas, ngaymuon, ngaytra, hantra, ghichu);
+                MessageBox.Show("Sửa thành công!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mapm = cbbPhieu.Text.Trim();
+                string mas = cbbSach.Text.Trim();
+                MessageBox.Show("Bạn xác nhận muốn xóa ???", "Thông báo", MessageBoxButtons.YesNoCancel);
+                SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "delCTPM", mapm, mas);
+                MessageBox.Show("Xóa thành công!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
+        }
+
+
+    }
+}
---
+++ PhieumuonControl.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationBlocks.Data;
+using QLTV.Class;
+
+namespace QLTV
+{
+    public partial class PhieumuonControl : UserControl
+    {
+      
+        public PhieumuonControl()
+        {
+            InitializeComponent();
+            Load += loaddgv;
+        }
+        private void loaddgv(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+
+        private void loaddulieu()
+        {
+            dgvHienthi.DataSource = SqlHelper.ExecuteDataset(QLTV.Class.StrConnect.strConnect(), "loadphieumuon").Tables[0];
+        }
+
+        private void btCapnhat_Click(object sender, EventArgs e)
+        {
+            loaddulieu();
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mapm = txtMa.Text.Trim();
+                string mabd = cbbbandoc.Text.Trim();
+                string trangthai = txtTrangthai.Text.Trim();
+                SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "addPM", mapm, mabd, trangthai);
+                MessageBox.Show("Thêm thành công!");
+                loaddulieu();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string mapm = txtMa.Text.Trim();
+                string mabd = cbbbandoc.Text.Trim();
+                string trangthai = txtTrangthai.Text.Trim();
+                SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "updatePM", mapm, mabd, trangthai);
+                MessageBox.Show("Sửa thành công!");
+                loaddulieu();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mapm = txtMa.Text.Trim();
+                MessageBox.Show("Bạn xác nhận muốn xóa ???", "Thông báo", MessageBoxButtons.YesNoCancel);
+                SqlHelper.ExecuteNonQuery(StrConnect.strConnect(), "delPM", mapm);
+                MessageBox.Show("Xóa thành công!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi!!!");
+            }
+        }
+
+    }
+}
